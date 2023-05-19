51abdc8ba9cbc023e2b4d650ed7755edf28f7b4b
MordesMortes <59325226+MordesMortes@users.noreply.github.com>
2023-05-19 09:03:33 -0600

    added dancer to videos
    added dancer to videos
    
    commented out using UnityEditor.Events in OculusButtonActionComplete.cs as it was causing build errors

--- Assets/OculusControllerTutorial/Scripts/OculusButtonActionComplete.cs
+++ Assets/OculusControllerTutorial/Scripts/OculusButtonActionComplete.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
-using UnityEditor.Events;
+//using UnityEditor.Events;
 
 public class OculusButtonActionComplete : MonoBehaviour
 {
