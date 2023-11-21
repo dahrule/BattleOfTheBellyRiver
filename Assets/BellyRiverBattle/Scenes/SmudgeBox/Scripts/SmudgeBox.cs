5e989a83e8da3cbf3731f9e1468e3f7011d745ca
Daniel Ruiz <dahrule@hotmail.com>
2023-11-20 22:41:29 -0500

    smudgebox scene interaction complete
    needs a better smoke effect.

--- Assets/BellyRiverBattle/Scenes/SmudgeBox/Scripts/SmudgeBox.cs
+++ Assets/BellyRiverBattle/Scenes/SmudgeBox/Scripts/SmudgeBox.cs
@@ -1,53 +1,91 @@
 using UnityEngine;
+using UnityEngine.VFX;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class SmudgeBox : MonoBehaviour
 {
-    [SerializeField] XRSocketInteractor [] SmudgeItems;
-    [SerializeField] ParticleSystem smokeVfx;
-    [SerializeField] ParticleSystem fireVfx;
+    [SerializeField] XRSocketInteractor [] smudgeItemSockets;
+    [SerializeField] GameObject[] smudgeItemGameobjects;
+    [SerializeField] VisualEffect smokeVfx;
+    [SerializeField] float delaySeconds = 4f;
 
     private int smudgeItemCount = 0;
     private readonly int smudgeItemsNeeded=3;
 
+    private void Start()
+    {
+        smokeVfx.gameObject.SetActive(false);
+
+    }
+
     private void OnEnable()
     {
-        foreach(var item in SmudgeItems)
+        for (int i = 0; i < smudgeItemSockets.Length; i++)
         {
-            item.selectEntered.AddListener(AddSmudgeItem);
-            item.selectExited.AddListener(RemoveSmudgeItem);
+            int currentIndex = i;
+            smudgeItemSockets[currentIndex].selectEntered.AddListener((SelectEnterEventArgs args) => OnSelectEnteredCallback(args, currentIndex));
+            smudgeItemSockets[currentIndex].selectExited.AddListener((SelectExitEventArgs args) => OnSelectExitedCallback(args, currentIndex));
         }
     }
 
     private void OnDisable()
     {
-        foreach (XRSocketInteractor socket in SmudgeItems)
+        for (int i = 0; i < smudgeItemSockets.Length; i++)
         {
-            socket.selectEntered.RemoveListener(AddSmudgeItem);
-            socket.selectExited.RemoveListener(RemoveSmudgeItem);
+            int currentIndex = i;
+            smudgeItemSockets[currentIndex].selectEntered.RemoveListener((SelectEnterEventArgs args) => OnSelectEnteredCallback(args, currentIndex));
+            smudgeItemSockets[currentIndex].selectExited.RemoveListener((SelectExitEventArgs args) => OnSelectExitedCallback(args, currentIndex));
         }
     }
 
-    private void AddSmudgeItem(SelectEnterEventArgs arg0)
+    private void OnSelectEnteredCallback(SelectEnterEventArgs args, int currentIndex)
+    {
+        AddSmudgeItem();
+        HideGhostItem(smudgeItemGameobjects[currentIndex]);
+    }
+
+    private void OnSelectExitedCallback(SelectExitEventArgs args, int currentIndex)
+    {
+        RemoveSmudgeItem();
+        ShowGhostItem(smudgeItemGameobjects[currentIndex]);
+    }
+
+    private void AddSmudgeItem()
     {
         smudgeItemCount += 1;
 
         bool isMixComplete = smudgeItemCount == smudgeItemsNeeded;
         if (isMixComplete)
-            StartSmudgeFire();
+            Invoke(nameof(StartSmudgeFire), delaySeconds);
     }
 
-    private void RemoveSmudgeItem(SelectExitEventArgs arg0)
+    private void RemoveSmudgeItem()
     {
         smudgeItemCount -= 1;
+    }
+
+    private void HideGhostItem(GameObject ghostItem)
+    {
+        ghostItem.SetActive(false);
+    }
+
+    private void ShowGhostItem(GameObject ghostItem)
+    {
+        ghostItem.SetActive(true);
     }
 
     private void StartSmudgeFire()
     {
         Debug.Log("Starting smudge fire");
-        //Trigger smoke 
+        smokeVfx.gameObject.SetActive(true);
         //play a success sound
-        //add a mechanismo to avoid retriggering the event twice starign fire
+        this.enabled = false;
     }
 
 }
+
+
+
+
+
+
